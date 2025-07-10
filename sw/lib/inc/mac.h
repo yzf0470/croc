0b7d0ea1773751b3a766f60ab278f77d6a8a4920
Yannis Bolliger <yannis.bolliger@protonmail.com>
2025-07-10 21:34:43 +0200

    fix: sw: hinder compiler from reusing rs1/rs2 as rd for mac instruction

--- sw/lib/inc/mac.h
+++ sw/lib/inc/mac.h
@@ -8,6 +8,6 @@
 // R type for gnu assembler: opcode, func3, func7, rd, rs1, rs2
 #define MAC(a, b, c)                                                           \
   asm volatile(".insn r %1, %2, %3, %0, %4, %5"                                \
-               : "=r"(a)                                                       \
+               : "+&r"(a)                                                      \
                : "i"(MAC_OPCODE), "i"(MAC_FUNCT3), "i"(MAC_FUNCT7), "r"(b),    \
                  "r"(c))
